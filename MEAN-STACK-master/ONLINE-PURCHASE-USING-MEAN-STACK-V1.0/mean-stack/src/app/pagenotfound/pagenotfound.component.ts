399c66def2b0e76e2c49dce7b3706c3d0c42d17d
ShabnamSheikh <sheikh1151993@gmail.com>
2020-07-30 10:39:13 +0530

    mean stack

---
+++ MEAN-STACK-master/ONLINE-PURCHASE-USING-MEAN-STACK-V1.0/mean-stack/src/app/pagenotfound/pagenotfound.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagenotfound',
+  template: `
+    <p>
+     THIS PAGE DOES NOT EXIST!!!!!!!
+    </p>
+  `,
+  styles: []
+})
+export class PagenotfoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
